7db850e1d2e31978ab267aeefe5750b6c2ef98dc
sannimdev <inegg.apps@gmail.com>
2022-02-14 20:47:27 +0900

    Assignment 11

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
+import React, { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from 'expo-asset';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [loading, setLoading] = useState(true);
+    const onFinish = () => setLoading(false);
+    const preload = () => {
+        const fontsToLoad = [Ionicons.font];
+        const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+        const imagesToLoad = [require('./assets/logo.png')];
+        const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+        return Promise.all([...fontPromises, ...imagePromises]);
+    };
+
+    if (loading) {
+        return <AppLoading startAsync={preload} onFinish={onFinish} onError={console.warn} />;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Nomad Coffee!</Text>
+            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
